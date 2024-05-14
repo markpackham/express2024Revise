4f2a26377b4ffc4db137623b6293e304d9be6a09
mark <markpackham1@gmail.com>
2024-05-14 20:42:00 +0100

    Demo express working as static server for html
    https://youtu.be/CnH3kAXSrmU?si=u5DZfvPy0rK8U7DS&t=1207

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile();
-});
-
-// http://localhost:8000/about
-app.get("/about", (req, res) => {
-  res.send("About");
-});
+// setup static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(8000, () => {
   console.log(`Server is running on port 8000`);
