90d920a381f40fb7eb2bdb4da0347d9f030811bf
mark <markpackham1@gmail.com>
2024-05-15 15:46:08 +0100

    Add logger middleware
    https://youtu.be/CnH3kAXSrmU?si=uKSvSCnLtGp1iiv_&t=3548

--- routes/posts.js
+++ routes/posts.js
@@ -8,10 +8,20 @@ let posts = [
   { id: 4, title: "Post Four" },
 ];
 
+const logger = (req, res, next) => {
+  // Example of output
+  // GET http://localhost:8899/api/posts?limit=2
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  next();
+};
+
 // Get all posts
 // http://localhost:8899/api/posts
 // Limit
 // http://localhost:8899/api/posts?limit=2
+// router.get("/", logger, (req, res) => {
 router.get("/", (req, res) => {
   const limit = parseInt(req.query.limit);
 
