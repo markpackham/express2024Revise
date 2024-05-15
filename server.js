e4f17c435b56a0c632ea42d08eee7bae2f9c1f78
mark <markpackham1@gmail.com>
2024-05-15 08:42:39 +0100

    Get single post
    https://youtu.be/CnH3kAXSrmU?si=b2OSLxasNqa36vwL&t=1821

--- server.js
+++ server.js
@@ -16,10 +16,21 @@ let posts = [
   { id: 4, title: "Post Four" },
 ];
 
+// Get all posts
+// http://localhost:8899/api/posts
 app.get("/api/posts", (req, res) => {
   res.json(posts);
 });
 
+// Get single post
+// http://localhost:8899/api/posts/1
+app.get("/api/posts/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  res.json(posts.filter((post) => post.id === id));
+});
+
+//
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
