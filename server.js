4e6b036a9f64ed8904397e22c128dfb52218f6f7
mark <markpackham1@gmail.com>
2024-05-15 14:38:11 +0100

    Use return statements instead of "else"
    https://youtu.be/CnH3kAXSrmU?si=0tSmoL9xaIfFDLyf&t=2257

--- server.js
+++ server.js
@@ -24,10 +24,9 @@ app.get("/api/posts", (req, res) => {
   const limit = parseInt(req.query.limit);
 
   if (!isNaN(limit) && limit > 0) {
-    res.status(200).json(posts.slice(0, limit));
-  } else {
-    res.status(200).json(posts);
+    return res.status(200).json(posts.slice(0, limit));
   }
+  res.status(200).json(posts);
 });
 
 // Get single post
@@ -36,16 +35,13 @@ app.get("/api/posts/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
   const post = posts.find((post) => post.id === id);
-  //res.status(200).json(posts.filter((post) => post.id === id));
 
   if (!post) {
-    res.status(404).json({ msg: `That post was not found` });
-  } else {
-    res.status(200).json(post);
+    return res.status(404).json({ msg: `That post was not found` });
   }
+  res.status(200).json(post);
 });
 
-//
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
