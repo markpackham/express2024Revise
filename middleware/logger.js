ca5a436dce0af7e7c7953b720ea69d8b1930e47d
mark <markpackham1@gmail.com>
2024-05-16 15:28:15 +0100

    Add specific log color to request type
    https://youtu.be/CnH3kAXSrmU?si=1tZUp_-t8y-y2UXF&t=4459

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,8 +1,21 @@
+import colors from "colors";
+
 const logger = (req, res, next) => {
+  const methodColors = {
+    GET: "green",
+    POST: "blue",
+    PUT: "yellow",
+    DELETE: "red",
+  };
+
+  const color = methodColors[req.method] || white;
+
   // Example of output
   // GET http://localhost:8899/api/posts?limit=2
   console.log(
-    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`[
+      color
+    ]
   );
   next();
 };
