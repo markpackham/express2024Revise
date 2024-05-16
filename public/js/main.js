40a947a1f0d8558f74da17367d0cb0e05ab44301
mark <markpackham1@gmail.com>
2024-05-16 16:21:55 +0100

    Add show posts function
    https://youtu.be/CnH3kAXSrmU?si=wEsh58QoPdpWRahM&t=5358

--- public/js/main.js
+++ public/js/main.js
@@ -2,16 +2,22 @@ const output = document.querySelector("#output");
 const button = document.querySelector("#get-posts-btn");
 
 async function showPosts() {
-  const res = await fetch(`http://localhost:8000/api/posts`);
-  if (!res.ok) {
-    throw new Error("Failed to fetch posts");
+  try {
+    const res = await fetch(`http://localhost:8000/api/posts`);
+    if (!res.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+
+    const posts = await res.json();
+    output.innerHTML = "";
+
+    posts.forEach((post) => {
+      // Post element
+      const postEl = document.createElement("div");
+      postEl.textContent = post.title;
+      output.appendChild(postEl);
+    });
+  } catch (error) {
+    console.log("Error fetching posts: ", error);
   }
-
-  const posts = await res.json();
-  output.innerHTML = "";
-
-  posts.forEach((post) => {
-    // Post element
-    const postEl = document.createElement("div");
-  });
 }
