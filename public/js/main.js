19b33a055aa1874d42b04688e8384492a8a8e48a
mark <markpackham1@gmail.com>
2024-05-16 16:17:38 +0100

    Add main.js to help show posts on frontend
    https://youtu.be/CnH3kAXSrmU?si=gm6Mz49TN5rygDu4&t=5300

---
+++ public/js/main.js
@@ -0,0 +1,17 @@
+const output = document.querySelector("#output");
+const button = document.querySelector("#get-posts-btn");
+
+async function showPosts() {
+  const res = await fetch(`http://localhost:8000/api/posts`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch posts");
+  }
+
+  const posts = await res.json();
+  output.innerHTML = "";
+
+  posts.forEach((post) => {
+    // Post element
+    const postEl = document.createElement("div");
+  });
+}
