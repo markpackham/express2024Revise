0db2ae43fcf6c381b0a6a209b68a0487a7a8c728
mark <markpackham1@gmail.com>
2024-05-16 16:30:57 +0100

    Get the posts
    https://youtu.be/CnH3kAXSrmU?si=ZqkC9GbI-jtQY_PE&t=5386

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ const button = document.querySelector("#get-posts-btn");
 
 async function showPosts() {
   try {
-    const res = await fetch(`http://localhost:8000/api/posts`);
+    const res = await fetch(`http://localhost:8899/api/posts`);
     if (!res.ok) {
       throw new Error("Failed to fetch posts");
     }
@@ -21,3 +21,6 @@ async function showPosts() {
     console.log("Error fetching posts: ", error);
   }
 }
+
+// Event listeners
+button.addEventListener("click", showPosts);
